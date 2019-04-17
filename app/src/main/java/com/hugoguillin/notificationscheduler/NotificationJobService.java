f6f40a878389630d15e255af93f02f60a3aa0c9e
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-17 13:41:06 +0200

    Task_1

---
+++ app/src/main/java/com/hugoguillin/notificationscheduler/NotificationJobService.java
@@ -0,0 +1,59 @@
+package com.hugoguillin.notificationscheduler;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationJobService extends JobService {
+
+    NotificationManager manager;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        createNotificationChannel();
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Job Service")
+                .setContentText("Your Job is running")
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_job_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+
+        manager.notify(0, builder.build());
+        return false;
+
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return true;
+    }
+
+    public void createNotificationChannel(){
+        manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel canal = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Job Service notification", NotificationManager.IMPORTANCE_HIGH);
+
+            canal.enableLights(true);
+            canal.setLightColor(Color.RED);
+            canal.enableVibration(true);
+            canal.setDescription(getApplicationContext().getString(R.string.descripcion_canal));
+            manager.createNotificationChannel(canal);
+        }
+    }
+}
